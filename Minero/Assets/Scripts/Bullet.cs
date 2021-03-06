b9a013ec804d53d98ca78a75500b264da69050a2
bryan christiano <bryantrack1@gmail.com>
2021-03-05 21:28:36 -0800

    autism
    game1

---
+++ Minero/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddRelativeForce(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
